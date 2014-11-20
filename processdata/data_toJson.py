3157e78ff336db78cdb07d77a126e3daae5673f9
Kiante Brantley <kiante.brantley@gmail.com>
2014-11-20 15:35:39 -0500

    Updated the name to json script and the json file with the document to color mapping

--- processdata/data_toJson.py
+++ processdata/data_toJson.py
@@ -7,17 +7,18 @@
 
 import json
 import csv
+import sys
 import random
 from colorsys import hsv_to_rgb
 from random import randint, uniform
 
 def main():
-    csvfile = open('data.csv', 'r')
+    csvfile = open(sys.argv[1], 'r')
     jsonfile = open('data_names.json', 'w')
     names_with_color = {}
     r = lambda: random.randint(0,255)
 
-    csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    csvreader = csv.reader(csvfile, delimiter='"', quotechar='|')
     count = 0
     first_line = True
     for row in csvreader:
@@ -25,6 +26,7 @@ def main():
             first_line = False
             continue 
 
+        row = row[2].split(",")
         h = uniform(0.25, 0.38) # Select random green'ish hue from hue wheel
         s = uniform(0.2, 1)
         v = uniform(0.3, 1)
@@ -33,7 +35,7 @@ def main():
 
         # Convert to 0-1 range for HTML output
         r, g, b = [x*255 for x in (r, g, b)]
-        names_with_color[row[0]] = [randint(100,300),randint(0,100),randint(0,80)]
+        names_with_color[row[1]] = [randint(100,300),randint(0,100),randint(0,80)]
         count+= 1
 
     json.dump(names_with_color,jsonfile)
