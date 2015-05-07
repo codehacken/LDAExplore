bae2ed606243e258d5add15c09857abc14a00f4c
Codehacken <codehacken@gmail.com>
2015-05-07 19:39:38 -0400

    Revert "Li-Chien"
    This reverts commit 1be490e04897a339c2476b91f7b2395e40528a37.

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-CMSC691-FINALPROJECT
+LDAExplore
 ====================
 
-This is the FINAL Project code for CMSC 691.
 The project uses the lda library from python which implements Latent Dirichlet Allocation (LDA) using Gibbs Sampling.
 
 DOCUMENTATION
--- processdata/lda.py
+++ processdata/lda.py
@@ -7,11 +7,10 @@ Basic LDA module that is used in the project.
 import re
 from gensim import corpora, models
 import operator
-import numpy  # for top_word re-rank
-import scipy  # for gmean
+
 
 class LDAVisualModel:
-    def __init__(self, word_corpus, scaling_factor=100000):
+    def __init__(self, word_corpus):
         """
         The LDAVisualModel requires list of word lists from the
         document corpus. Each list of words represents a document.
@@ -20,9 +19,8 @@ class LDAVisualModel:
         self.id2word = corpora.Dictionary(word_corpus)
         self.mm = []
         self.lda = None
-        self.scaling_factor = scaling_factor
 
-    def create_word_corpus(self, word_corpus, store_corpus=False, store_loc='server/data/TVCG_corpus.mm'):
+    def create_word_corpus(self, word_corpus, store_corpus=False, store_loc='dicts/corpus.mm'):
         """
         :param word_corpus: word_corpus: [[<words>],[],[]]
         :param store_corpus: boolean to store the serialized corpus or not.
@@ -30,28 +28,19 @@ class LDAVisualModel:
         """
         for text in word_corpus:
             self.mm.append(self.id2word.doc2bow(text))
-	
+
         if store_corpus:
             corpora.MmCorpus.serialize(store_loc, word_corpus)
 
-    def train_lda(self, num_top=10, alpha1='symmetric', num_pass=1, update_t=1, chunks=2000, num_iter=50,
-                  num_eval=10, gamma_t=0.001, kappa=0.5, tao=1.0, eta=None):
+    def train_lda(self, num_top=2, update_t=1, chunks=10000, num_pass=1):
         """
         :param num_top: The number of topics for which LDA trains.
         :param update_t:
         :param chunks:
         :param num_pass: The number of passes that LDA executes on the data.
         """
-        self.lda = models.LdaModel(corpus=self.mm, id2word=self.id2word,
-                                   num_topics=num_top, alpha=alpha1,
-                                   passes=num_pass, update_every=update_t,
-                                   chunksize=chunks, iterations=num_iter,
-                                   eval_every=num_eval, gamma_threshold=gamma_t,
-                                   decay=kappa, offset=tao, eta=eta)
-        #(corpus=None, num_topics=100, id2word=None, distributed=False,
-        # chunksize=2000, passes=1, update_every=1, alpha='symmetric',
-        # eta=None, decay=0.5, offset=1.0, eval_every=10, iterations=50,
-        # gamma_threshold=0.001)
+        self.lda = models.LdaModel(corpus=self.mm, id2word=self.id2word, num_topics=num_top,
+                                   update_every=update_t, chunksize=chunks, passes=num_pass)
 
     def get_lda_corpus(self, num_of_topics=10, num_of_words=10):
         """
@@ -71,12 +60,11 @@ class LDAVisualModel:
 
         # Build the topic - document matrix.
         doc_top = []
-        for idx, doc in enumerate(self.lda[self.mm]): # idx=0; doc=[(2,0.018),(5,0.974)]
-                                                      # idx=4; doc=[(5,0.984)]
-            doc_top.append([0] * num_topics)       # [[0,0,0,0,0,0,0,0,0,0]]
+        for idx, doc in enumerate(self.lda[self.mm]):
+            doc_top.append([0] * num_topics)
             for topic in doc:
-                doc_top[idx][topic[0]] = topic[1]  # topic = (2, 0.0182)
-                                                   # doc[idx,2] = 0.0182
+                doc_top[idx][topic[0]] = topic[1]
+
         return doc_top
 
     def generate_doc_topic_rank(self):
@@ -97,7 +85,7 @@ class LDAVisualModel:
             # Construct the ranks.
             prob_rank = sorted(top_prob, reverse=True)
             top_sort = sorted(range(len(top_prob)), key=lambda k: top_prob[k],
-                              reverse=True) # [3, 7, 0, 1, ...] --> (Largest) 3->7->...
+                              reverse=True)
 
             # Create a new list with the ranks.
             for rank, topic in enumerate(top_sort):
@@ -109,66 +97,6 @@ class LDAVisualModel:
             doc_top_rank.append(top_rank)
 
         return doc_top_rank
-
-    def generate_doc_word(self, doc_top):
-        top_word = self.lda.expElogbeta # topic*word 2d array matrix
-        doc_word = [[0 for i in range(len(top_word[0]))] for j in range(len(doc_top))]
-        for i, doc in enumerate(doc_top):
-            for j, word in enumerate(top_word[0]):
-                for k, topic in enumerate(doc):
-                    doc_word[i][j] = doc_word[i][j] + doc_top[i][k] * top_word[k][j]
-
-        return doc_word
-
-    def generate_top_words(self, num_word):
-
-        top_matrix = self.lda.expElogbeta  # top_word_matrix
-
-    # Convert scores to rankings
-        top_word_index = [[0 for i in range(len(top_matrix[0]))] for j in range(len(top_matrix))]
-        for i, top_word in enumerate(top_matrix):  # CHANGE to top_matrix to test correctness
-            top_word_index[i] = numpy.argsort(top_word)[::-1]
-       # self.lda.expElogbeta: the topic*word distribution matrix
-       # self.id2word.id2token[1]: first word in dictionary
-
-    # Convert rankings to topic-word list
-        top_word_list = []
-
-        for i, word_list in enumerate(top_word_index):
-            top_word = []
-            for j, word_id in enumerate(word_list):
-                if j == num_word:
-                    break
-                keyword_information = "%s (%f)" % (self.id2word.id2token[word_id], top_matrix[i][word_id])
-                top_word.append(keyword_information) #self.id2word.id2token[word_id]) #CHANGED FOR DEBUGGING
-            top_word_list.append(top_word)
-
-        return top_word_list, top_word_index
-
-    @staticmethod
-    def gen_topic_hierarchy(topics):
-        # Create the hierarchy.
-        top_word_hier = {"children": []}
-
-        for idx, word_list in enumerate(topics):
-            children = []
-
-            # Create the children nodes - words.
-            top_word_str = ""
-            i = 0
-
-            for word in word_list:
-                if i < 3:
-                    top_word_str += word[1]
-                    i += 1
-
-                children.append({"name": word[1], "size": word[0],
-                                 "value": float(word[0])*1000})
-            # Add the list to current hierarchy.
-            top_word_hier["children"].append({"name": "T" + str(idx), "children": children})
-
-        top_word_hier["name"] = 'LDA Topic Modeling'
-        return top_word_hier
 
     @staticmethod
     def gen_doc_top_words(topics, doc_top):
@@ -193,125 +121,3 @@ class LDAVisualModel:
             doc_to_word.append(sorted_word_prob)
 
         return doc_to_word
-
-    @staticmethod
-    def gen_topic_words(topics):
-        top_word_list = []
-       # top_word_index_list = []
-        for idx, word_list in enumerate(topics):
-            top_word = []
-       #     top_word_index = []
-
-            for word in word_list:
-                top_word.append(word[1])
-       #         top_word_index.append(self.lda.id2word.token2id)
-
-            # Add the list to current hierarchy.
-            top_word_list.append(top_word)
-       #     top_word_index_list.append(top_word_index)
-
-        return top_word_list  #, top_word_index_list
-
-    """ ================ Post-processing ================ """
-
-    def top_word_re_rank(self, num_word, re_rank_method):
-
-        top_matrix = self.lda.expElogbeta  # top_word_matrix
-        if re_rank_method == 1:
-            kr_denominator = numpy.sum(top_matrix, axis=0)  # kr: keyword re-ranking
-
-            kr_score = [[0 for i in range(len(top_matrix[0]))] for j in range(len(top_matrix))]
-            for i, topic_dis in enumerate(top_matrix):
-                for j, prob_word in enumerate(topic_dis):
-                    kr_score[i][j] = prob_word / kr_denominator[j]  # keyword re-ranking score
-        else:
-            kr_denominator = scipy.stats.mstats.gmean(top_matrix, axis=0)  # kr: keyword re-ranking
-
-            kr_score = [[0 for i in range(len(top_matrix[0]))] for j in range(len(top_matrix))]
-            for i, topic_dis in enumerate(top_matrix):
-                for j, prob_word in enumerate(topic_dis):
-                    kr_score[i][j] = prob_word * numpy.log( prob_word / kr_denominator[j])  # keyword re-ranking score
-
-    # Convert scores to rankings
-        kr_rank = [[0 for i in range(len(top_matrix[0]))] for j in range(len(top_matrix))]
-        for i, topic_score in enumerate(kr_score):  # CHANGE to top_matrix to test correctness
-            kr_rank[i] = numpy.argsort(topic_score)[::-1]
-       # self.lda.expElogbeta: the topic*word distribution matrix
-       # self.id2word.id2token[1]: first word in dictionary
-
-    # Convert rankings to topic-word list
-        top_word_list = []
-
-        for i, word_list in enumerate(kr_rank):
-            top_word = []
-            for j, word_id in enumerate(word_list):
-                if j == num_word:
-                    break
-                keyword_information = "%s (%f -> %f)" % (self.id2word.id2token[word_id],top_matrix[i][word_id],kr_score[i][word_id])
-                top_word.append(self.id2word.id2token[word_id])  #keyword_information) #CHANGED FOR DEBUGGING
-            top_word_list.append(top_word)
-
-        return top_word_list  # re_top_word
-
-    def doc_word_index_set(self):
-        index_set = [0 for i in range(len(self.mm))]
-        for i, doc in enumerate(self.mm):
-            index_set[i] = set()
-            for word in doc:
-                index_set[i].add(word[0])
-        return index_set
-
-    @staticmethod
-    def topic_coherence(top_word, num_word, doc_word_index_set):
-        num_topic = len(top_word)
-        coherence = [0 for i in range(num_topic)]
-        for t, topic in enumerate(top_word):
-            for i in range(1, num_word-1):  # m
-                for j in range(0, i-1):  # l
-                    D = 0
-                    DD = 0
-                    word_m_idx = topic[i]
-                    word_l_idx = topic[j]
-                    for doc in doc_word_index_set:
-                        if word_l_idx in doc:
-                            D = D + 1
-                            if word_m_idx in doc:
-                                DD = DD + 1
-                    coherence[t] = coherence[t] + numpy.log((DD+1)/D)
-
-        return coherence
-
-    @staticmethod
-    def perplexity(prob_doc_word, doc_word_index_set):
-        log_likelihood = 0
-        total_num_word = 0
-        for idx, prob_doc in enumerate(prob_doc_word):
-            p_d = 1
-            sorted_prob_doc = numpy.sort(prob_doc)[::-1]
-            for i in range(50):
-                p_d = p_d * sorted_prob_doc[i]
-            log_likelihood = log_likelihood + numpy.log(p_d)
-            total_num_word = total_num_word + len(doc_word_index_set[idx])
-        perplexity = numpy.exp(-log_likelihood / total_num_word)
-        return perplexity
-
-    # Another tool which uses Gibbs sampling
-    def mm_to_doc_word_times(self):
-        doc_word_times = [[0 for i in range(len(self.id2word.token2id))] for j in range(len(self.mm))]
-        for i, line in enumerate(self.mm):
-            for pair in line:
-                doc_word_times[i][pair[0]] = pair[1]
-        doc_word_times.shape = (len(self.mm), len(self.id2word.token2id))
-        return doc_word_times
-
- #   def train_clda(self, num_top=10, must_link, cannot_link, num_pass=1, update_t=1, chunks=2000, num_iter=50,
- #                 num_eval=10, gamma_t=0.001, kappa=0.5, tao=1.0, eta=None):
-
- #       alpha1 = 1 / num_top
-
- #       self.lda = models.LdaModel(corpus=self.mm, id2word=self.id2word,
- #                                  num_topics=num_top, alpha=alpha1,
- #                                  passes=num_pass, update_every=update_t,
- #                                  chunksize=chunks, iterations=num_iter,
- #                                  eval_every=num_eval, gamma_threshold=gamma_t,
- #                                  decay=kappa, offset=tao, eta=eta)
